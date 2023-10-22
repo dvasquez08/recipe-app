fe4d34dea933b2626a40c82ccca68381174b9c7e
dvasquez08 <dvasquez.422@gmail.com>
2023-10-22 02:18:01 -0600

    modified js code, a little bit of css and html to fix the search bug

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const ingredientInput = document.getElementById("ingredients");
-  const searchButtom = document.getElementById("search-button");
-  const recipeList = document.getElementById("recipe-list");
+  const ingredientInput = document.getElementById('ingredients');
+  const searchButton = document.getElementById('search-button');
+  const recipeList = document.getElementById('recipe-list');
 
-  searchButtom.addEventListener("click", function () {
-    const items = ingredientInput.value;
-    if (items.trim() === "") {
-      recipeList.innerHTML = "Please enter ingredients";
+  searchButton.addEventListener('click', function () {
+    const ingredients = ingredientInput.value;
+    if (ingredients.trim() === '') {
+      recipeList.innerHTML = 'Please enter ingredients';
     } else {
-      recipeList.innerHTML = "Searching for recipes...";
-      const apiKey = "3c8a4c31879e41d18d7c211b35fddc69";
-      `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
+      recipeList.innerHTML = 'Searching for recipes...';
+      const apiKey = '3c8a4c31879e41d18d7c211b35fddc69';
+      const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${apiKey}&ingredients=${ingredients}`;
 
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
           if (data.length === 0) {
-            recipeList.innerHTML = "No reciptes found.";
+            recipeList.innerHTML = 'No reciptes found.';
           } else {
             showRecipes(data);
           }
         })
         .catch((error) => {
           console.error(error);
-          recipeList.innerHTML = "An error occured while obtaining reciptes";
+          recipeList.innerHTML = 'An error occured while obtaining recipes';
         });
     }
   });
 
   function showRecipes(recipes) {
-    recipeList.innerHTML = "";
+    recipeList.innerHTML = '';
 
-    const ul = document.createElement("ul");
+    const ul = document.createElement('ul');
     recipes.forEach((recipe) => {
-      const li = document.createElement("li");
-      li.textContent = `${recipe.title} - Ingredients: ${recipe.items.join(
-        ", "
-      )}`;
+      const li = document.createElement('li');
+      li.textContent = `${
+        recipe.title
+      } - Ingredients: ${recipe.ingredients.join(', ')}`;
       ul.appendChild(li);
     });
 
